39371f287e010f6923776aecd4df15acdb2b4478
Sujoy Sett <sujoysett@gmail.com>
2015-06-13 20:14:09 +0530

    Update db-util.js

--- db-util.js
+++ db-util.js
@@ -1,19 +1,14 @@
 var mysql = require('mysql');
-var q= require('q');
-
+var q = require('q');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root12345',
     database: 'rwncv1'
 });
-
-
-exports.getuser=function(){
-var deffered=q.defer();
-var users=[];
-connection.connect();
-
+exports.getUsers = function() {
+    var deffered = q.defer();
+    var users = [];
     connection.query('SELECT user_name from user', function(err, rows, fields) {
         if (!err) {
             console.log('Query response is: ', rows);
@@ -26,9 +21,17 @@ connection.connect();
             console.log('Error while performing Query.');
         }
     });
-    connection.end();
-
-
-return deffered.promise;
-
-}
+    return deffered.promise;
+};
+exports.putUser = function(data) {
+    var deffered = q.defer();
+    var result;
+    connection.query('insert into user (user_name,user_pwd) values("' + data.name + '","' + data.pwd + '")', function(err, rows, fields) {
+        if (!err) {
+	    deffered.resolve('posted');
+        } else {
+            console.log('Error while performing Query.');
+        }
+    });
+    return deffered.promise;
+};
